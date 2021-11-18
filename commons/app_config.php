4eb76d1a13c2200e0e5073086564c69b1e3c0221
Tungduong2001 <94053977+Tungduong2001@users.noreply.github.com>
2021-11-18 11:22:47 +0700

    Update app_config.php

--- commons/app_config.php
+++ commons/app_config.php
@@ -1,10 +1,10 @@
 <?php
 
-const BASE_URL = "http://localhost/da1-nhom4/";
+const BASE_URL = "http://localhost:81/da1-nhom4/";
 
-const ADMIN_URL = "http://localhost/da1-nhom4/cp-admin/";
+const ADMIN_URL = "http://localhost:81/da1-nhom4/cp-admin/";
 const ADMIN_ASSETS = BASE_URL . 'public/adminlte/';
-const PUBLIC_ASSETS = "http://localhost/da1-nhom4/public/";
+const PUBLIC_ASSETS = "http://localhost:81/da1-nhom4/public/";
 
 
 ?>
