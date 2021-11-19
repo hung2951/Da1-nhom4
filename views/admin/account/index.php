d2caecb12ba25a279d85b2f10467307c7ba1bd98
Tungduong2001 <94053977+Tungduong2001@users.noreply.github.com>
2021-11-19 13:13:26 +0700

    a

--- views/admin/account/index.php
+++ views/admin/account/index.php
@@ -19,7 +19,7 @@
                 <td>
                     <img src="<?= PUBLIC_ASSETS . $u['image'] ?>" width="100">
                 </td>
-                <td><?=number_format($u['phone']) ?></td>
+                <td><?=$u['phone']?></td>
                 <td>
                     <a href="<?= ADMIN_URL . 'tai-khoan/sua?id=' . $u['id_user'] ?>" class="btn btn-sm btn-info">Sửa</a>
                     <a href="<?= ADMIN_URL . 'tai-khoan/xoa?id=' . $u['id_user'] ?>" class="btn btn-sm btn-danger">Xóa</a>
