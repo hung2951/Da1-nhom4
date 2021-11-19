4873a53e6d0669fccb3fbbcc69638be17e1daad3
Tungduong2001 <94053977+Tungduong2001@users.noreply.github.com>
2021-11-19 20:41:45 +0700

    quản lí đơn hàng adimin+ client

--- business/client/orders.php
+++ business/client/orders.php
@@ -1,10 +1,12 @@
 <?php
     function list_orders(){
         $id=$_GET['id'];
-        $sql= "select * from product p
+        $sql= "select *from product p
                 inner join orders o on o.id_product = p.id_product
                 inner join user u on o.id_user = u.id_user
-                where u.id_user=$id";
+                where u.id_user=$id
+                order by o.status asc";
+                
         $orders=executeQuery($sql);
 
         client_render('orders/index.php', [
--- views/client/orders/index.php
+++ views/client/orders/index.php
@@ -1,22 +1,29 @@
-<h2>Quản lí đơn hàng</h2>
-<a href="">
-    <div>
-        <?php foreach ($dsHoadon as $o) : ?>
-            <div class="orders">
-                <div class="order-image">
-                    <p><img src="<?= PUBLIC_ASSETS . '/uploads/avatars/' . $o['product_image'] ?>" width="100"></p>
-                </div>
-                <div class="order-info">
-                    <p><?= $o['product_name'] ?></p>
-                    <p>Size: <?= $o['size'] ?></p>
-                    <p>X<?= $o['quantity_orders'] ?></p>
-                    <p>Đơn giá: <?= $o['price'] ?></p>
-                </div>
+<div class="container">
+    <?php foreach ($dsHoadon as $o) : ?>
+        <div class="orders">
+            <div class="order-title">
+                <span>ID ĐƠN HÀNG: <?= $o['id_orders'] ?> | <?= $o['status'] == 0 ? "ĐƠN HÀNG ĐANG GIAO ĐẾN BÊN BẠN" : "ĐƠN HÀNG ĐÃ GIAO" ?></span>
             </div>
-            <p>Tổng tiền: <?=number_format($o['money']) ?> VNĐ</p>
             <a href="">
-                <p>Mua lại</p>
+                <div class="order">
+                    <div class="order-image">
+                        <p><img src="<?= PUBLIC_ASSETS . '/uploads/avatars/' . $o['product_image'] ?>" width="130px"></p>
+                    </div>
+                    <div class="order-info">
+                        <p id="order-info-name"><?= $o['product_name'] ?></p>
+                        <p>Size: <?= $o['size'] ?></p>
+                        <p>x<?= $o['quantity_orders'] ?></p>
+                        <p id="order-info-price">Đơn giá: <span><?= number_format($o['price']) ?></span><u>đ</u></p>
+                    </div>
+                </div>
+                <div class="money">
+                    <p>Tổng tiền: </p>
+                    <p><span> <?= number_format($o['money']) ?> <u>đ</u></span></p>
+                </div>
             </a>
-        <?php endforeach ?>
-    </div>
-</a>
+            <div class="buy">
+                <a href="" id="buys"> <p>Mua lại</p> </a>
+            </div>
+        </div>
+    <?php endforeach ?>
+</div>
