4a79b674e9580c5d59075d39dfd285c775a0c170
Tramnvph14967 <94088002+Tramnvph14967@users.noreply.github.com>
2021-11-29 16:26:03 +0700

    update

--- business/client/product-detail.php
+++ business/client/product-detail.php
@@ -1,11 +1,9 @@
 <?php
-
 function product_detail(){
     //lấy ra thông tin sản phẩm
     $id = $_GET['id'];
     $sql = "select * from product where id_product  = $id";
     $product_detail = executeQuery($sql, false);
-
 
     // Lấy ra sản phẩm theo danh mục
     $id_brand = $_GET['id_brand'];
@@ -15,6 +13,7 @@ function product_detail(){
     //lấy ra bình luận
     $sql = "SELECT * FROM comment bl INNER JOIN user kh ON bl.id_user = kh.id_user WHERE id=?";
     $comment = executeQuery($sql);
+    var_dump($comment);
 
     client_render('product-detail/index.php', [
         'product_detail' => $product_detail,
