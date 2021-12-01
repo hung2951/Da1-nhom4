ad69c7630171083688f1bced30d9cc83554cd976
hung2951 <94148492+hung2951@users.noreply.github.com>
2021-12-01 23:39:36 +0700

    login thành công

--- business/admin/account.php
+++ business/admin/account.php
@@ -1,5 +1,6 @@
 <?php
-function account_index(){
+function account_index()
+{
     $sql = "select * from user";
     $users = executeQuery($sql);
 
@@ -8,7 +9,8 @@ function account_index(){
     ]);
 }
 
-function account_remove(){
+function account_remove()
+{
     // lấy id từ đường dẫn
     $id = $_GET['id'];
     // thực thi câu lệnh xóa dựa vào id
@@ -17,39 +19,44 @@ function account_remove(){
     header("location: " . ADMIN_URL . 'tai-khoan');
 }
 
-function account_add_form(){
+function account_add_form()
+{
 
     admin_render('account/add-form.php');
 }
-function account_save_add(){
-    // nhận dữ liệu từ form gửi lên
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    $phone=$_POST['phone'];
-    // mã hóa mật khẩu
-    $passwordHash = password_hash($password, PASSWORD_DEFAULT);
-    // lưu ảnh vào thư mục public/uploads
-    $file = $_FILES['image'];
-    $avatar = "";
-    // Lưu ảnh
-    if($file['size'] > 0){
-        $filename = uniqid() . '-' . $file['name'];
-        move_uploaded_file($file['tmp_name'], './public/uploads/avatars/' . $filename);
-        $avatar = "uploads/avatars/" . $filename;
-    }
+function account_save_add()
+{
+    
+        // nhận dữ liệu từ form gửi lên
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $phone = $_POST['phone'];
+        // mã hóa mật khẩu
+        $passwordHash = password_hash($password, PASSWORD_DEFAULT);
+        // lưu ảnh vào thư mục public/uploads
+        $file = $_FILES['image'];
+        $avatar = "";
+        // Lưu ảnh
+        if ($file['size'] > 0) {
+            $filename = uniqid() . '-' . $file['name'];
+            move_uploaded_file($file['tmp_name'], './public/uploads/avatars/' . $filename);
+            $avatar = "uploads/avatars/" . $filename;
+        }
 
-    // tạo ra câu sql insert tài khoản mới
-    $sql = "insert into user
-                (full_name, email,phone, password, image) 
+        // tạo ra câu sql insert tài khoản mới
+        $sql = "insert into user
+                (full_name, email,phone, password, avatar) 
             values 
                 ('$name', '$email', '$phone','$passwordHash', '$avatar')";
-    // Thực thi câu sql với db
-    executeQuery($sql);
-    header("location: " . ADMIN_URL . 'tai-khoan');
+        // Thực thi câu sql với db
+        executeQuery($sql);
+        header("location: " . ADMIN_URL . 'tai-khoan');
+   
 }
 
-function account_edit_form(){
+function account_edit_form()
+{
     $id = $_GET['id'];
     $sql = "select * from user where id_user = $id";
     $user = executeQuery($sql, false);
@@ -58,7 +65,8 @@ function account_edit_form(){
     ]);
 }
 
-function account_save_edit(){
+function account_save_edit()
+{
     // lấy ra thông tin cũ của dữ liệu vừa submit lên
     $id = $_GET['id'];
     $sql = "select * from user where id_user = $id";
@@ -68,10 +76,10 @@ function account_save_edit(){
     $email = $_POST['email'];
     $phone = $_POST['phone'];
     // lưu ảnh vào thư mục public/uploads
-    $file = $_FILES['image'];
-    $avatar = $oldData['image'];
+    $file = $_FILES['avatar'];
+    $avatar = $oldData['avatar'];
     // Lưu ảnh
-    if($file['size'] > 0){
+    if ($file['size'] > 0) {
         $filename = uniqid() . '-' . $file['name'];
         move_uploaded_file($file['tmp_name'], './public/uploads/avatars/' . $filename);
         $avatar = "uploads/avatars/" . $filename;
@@ -83,12 +91,9 @@ function account_save_edit(){
                 full_name = '$name', 
                 email = '$email', 
                 phone = '$phone',
-                image = '$avatar' 
+                avatar = '$avatar' 
             where id_user = $id";
     // Thực thi câu sql với db
     executeQuery($sql);
-    header("location: " . ADMIN_URL . 'tai-khoan');
+header("location: " . ADMIN_URL . 'tai-khoan');
 }
-
-?>
-
--- business/client/login.php
+++ business/client/login.php
@@ -1,41 +1,28 @@
 <?php
 
-function login(){
+function login()
+{
     client_render('login/login.php');
 }
-function check_login(){
+function check_login()
+{
 
     // if (isset($_POST['btn-submit'])) {
-        $email = $_POST['email'];
-        $password = $_POST['password'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
 
-        $sql = "SELECT * FROM user WHERE email = '$email'";
-        $oldData = executeQuery($sql, false); 
-        if ($oldData) {
-            if ($email == $oldData['email']) {
-                if ($password == password_verify($password, $oldData['password']) ){
-                    $_SESSION['khach_hang'] = $oldData;
-                    header('http://localhost/da1-nhom4/');
-                } 
-            } else{
-                echo 'Mật khẩu không đúng';
-            }
+    $sql = "SELECT * FROM user WHERE email = '$email'";
+    $oldData = executeQuery($sql, false);
+    if ($oldData) {
+        if (password_verify($password, $oldData['password'])) {
+            $_SESSION['khach_hang'] = $oldData;
+            header('location: http://localhost/da1-nhom4/');
         }else{
-            echo 'Tài khoản không tồn tại';
+            echo "mật khẩu sai";
         }
-    
-    //     if(isset($_POST['email'])){
-    //         $password = password_verify($password, $oldData['password']);
-    //         if($password){
-    //             $_SESSION['user'] = $oldData;
-    //             header('location: ' .CLIENT_URL. 'index.php');
-    //         }else{
-    //             header('location: ' .CLIENT_URL. 'login/login.php');
-    //             echo 'Mật khẩu không đúng';
-    //         }
-    //     }else{
-    //         header('location: ' .CLIENT_URL. 'login/login.php');
-    //         echo 'Email không tồn tại';
-    //     }
-    // }
+    } else {
+        echo 'Tài khoản không tồn tại';
+    }
+
+
 }
