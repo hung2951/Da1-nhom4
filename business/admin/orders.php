eb2cd2c62577330e6ca13d44a0373207fa6d98a4
hung2951 <94148492+hung2951@users.noreply.github.com>
2021-12-02 23:44:32 +0700

    xóa hàm thừa

--- business/admin/orders.php
+++ business/admin/orders.php
@@ -1,34 +1,4 @@
 <?php
-function list_order()
-{
-    $id = $_GET['id'];
-    $sql = "select *from product p
-                    inner join orders o on o.id_product = p.id_product
-                    inner join user u on o.id_user = u.id_user
-                    where u.id_user=$id
-                    order by o.status asc";
-    $orders = executeQuery($sql);
-    admin_render('orders/list-orders.php', [
-        'dsHoaDon' => $orders,
-    ]);
-}
-function order_details()
-{
-    $id = $_GET['id'];
-    $sql_user = "select * from user u join orders o on u.id_user = o.id_user
-                            where u.id_user = o.id_user and  id_orders=$id";
-    $user = executeQuery($sql_user, false);
-
-    $sql_order_detail = "select * from orders o join product p on p.id_product = o.id_product
-                                 where id_orders=$id";
-    $order_detail = executeQuery($sql_order_detail, false);
-    admin_render('orders/order-detail.php', [
-        'u' => $user,
-        'o' => $order_detail,
-    ]);
-}
-
-
 
 function save_update_status()
 {
