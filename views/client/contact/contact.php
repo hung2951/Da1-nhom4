49e151ee20c8577d4bc7ca9bac529949760b94af
Tramnvph14967 <94088002+Tramnvph14967@users.noreply.github.com>
2021-12-05 15:15:53 +0700

    update_lien_he

---
+++ views/client/contact/contact.php
@@ -0,0 +1,77 @@
+<main class="ps-main">
+      <div class="ps-contact ps-section pb-80" style="padding:20px" >
+      
+        <div class="ps-container">
+          <div class="row">
+                <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 ">
+                  <div class="ps-section__header mb-50">
+                    <h2 class="ps-section__title" data-mask="Contact">- Get in touch</h2>
+                    <form class="ps-contact__form" action="<?= CLIENT_URL . 'lien-he/luu-tao-moi' ?>" method="post">
+                      <div class="row">   
+                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 ">
+                              <div class="form-group">
+                                <label>Name <sub>*</sub></label>
+                                <input name="name_contact" class="form-control" type="text" placeholder="">
+                              </div>
+                            </div>
+                            
+                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 ">
+                              <div class="form-group">
+                                <label>Số điện thoại<sub>*</sub></label>
+                                <input name="phone_contact" class="form-control" type="int" placeholder="">
+                              </div>
+                            </div>
+                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 ">
+                              <div class="form-group">
+                                <label>Email <sub>*</sub></label>
+                                <input name="email_contact" class="form-control" type="email" placeholder="">
+                              </div>
+                            </div>
+                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 ">
+                              <div class="form-group">
+                                <label><sub>*</sub></label>
+                                <input name="date_contact" class="form-control" type="date" value="<?= date('Y-m-d') ?>" placeholder="">
+                              </div>
+                            </div>
+                            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 ">
+                              <div class="form-group mb-25">
+                                <label>Your Message <sub>*</sub></label>
+                                <textarea name="content_contact" class="form-control" rows="6"></textarea>
+                              </div>
+                              <div class="form-group">
+                                <button type="submit" class="ps-btn">Send Message<i class="ps-icon-next"></i></button>
+                              </div>
+                            </div>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 ">
+                  <div class="ps-section__content">
+                    <div class="row">
+
+                          <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 ">
+                            <div class="ps-contact__block" data-mh="contact-block">
+                              <header>
+                                <h3>Shoes-store<span> Việt Nam</span></h3>
+                              </header>
+                              <footer>
+                                <p><i class="fa fa-map-marker"></i>Phố Trịnh Văn Bô, Xuân Phương, Nam Từ Liêm, Hà Nội, Vietnam</p>
+                                <p><i class="fa fa-envelope-o"></i><a href="mailto%40supportShoes%40shoes.html"><span class="__cf_email__" data-cfemail="added8ddddc2dfd9fec5c2c8deeddec5c2c8de83c3c8d9">CaodangFPT@fpt.edu.vn</span></a></p>
+                                <p><i class="fa fa-phone"></i>Hotline: 090-377-9999 / 804-399-3580</p>
+                              </footer>
+                            </div>
+                          </div>
+                          <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 ">
+                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.863981044334!2d105.74459841476343!3d21.03812778599329!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x313454b991d80fd5%3A0x53cefc99d6b0bf6f!2sFPT%20Polytechnic%20Hanoi!5e0!3m2!1sen!2s!4v1638690359989!5m2!1sen!2s" width="600" height="350" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                          </div>
+
+                    </div>
+                  </div>
+                </div>
+          </div>
+        </div>
+      </div>
+      
+    </main>
+ 
