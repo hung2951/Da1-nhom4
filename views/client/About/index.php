c2857a073c2d58f439d18ef028edf47b7b650299
dainxph14951 <94088122+dainxph14951@users.noreply.github.com>
2021-12-06 14:16:56 +0700

    updata

--- views/client/About/index.php
+++ views/client/About/index.php
@@ -1,1 +1,35 @@
-<h1>aaaaaaaaaaaaaaaaa</h1>
+<div class="learning-news" style="margin-bottom: 50px;">
+
+                <div class="container">
+                  <div class="introduce">
+                      <h3 style="text-align: center; margin-bottom: 30px;">SPOES STORE AND HANDHAKHAKET WAKES THE VIETNAM FASHION VILLAGE</h3>
+                      <p>As a domestic brand but always with a dream of reaching out to the world, SPOES STORE has been realizing this goal with concrete and full transformations...</p>
+                      <strong>As a domestic brand but always carrying a dream of reaching out to the world, SPOES STORE has been realizing this goal with concrete and bold transformations.</strong>
+                      <strong>Vietnamese fashion, Italian style</strong>
+                      <p>Born in 2013, SPOES STORE is considered as a brand with many breakthrough developments, in the context of men's fashion in Vietnam has not yet found a leader. As a domestic fashion brand, since its inception, Aristino has been shaped to become an international brand, choosing Italian style as the source "gene" for long-term strategy development.</p>
+                      <p>Understanding fashion is a signal of masculinity, where men express their needs, status, personality and aspirations, SPOES STORE has created elegant, luxurious, quality designs, skillful harmony between practicality and fashion. This helps Aristino really grow fast and solid. In the past 6 years, the brand has enjoyed a strong position in the world, showing a strong rise with more than 80 stores and hundreds of points of sale, regularly launching collections and continuously diversifying products. product line.</p>
+                      <strong>Not only selling clothes, SPOES STORE tells the story of men</strong>
+                      <p>Across its collections and products, SPOES STORE creates an inspirational story about the man in society, a general definition of "Be the man" - What is a real man.</p>
+                      <div class="image" style="text-align: center;">
+                        <img src="img/fff.jpg" alt="">
+                        <p>Actor Manh Truong is a loyal customer of SPOES STORE.</p>
+                      </div>
+                      <p>Not only bravery, talent, and success, it is also sophistication, romance, liberality and full of breakthroughs. It is what a man needs to assert himself, become an important factor in work, society and personal life. Aristino's product system supports a man's life in any social context, accompanying them to conquer the successes ahead.</p>
+                      <p>Product orientation with sophisticated, elegant and brave style, plus the expectation of bringing a new wind in design, SPOES STORE took a bold step in 2019 when inviting Italian designer Michele Gaudiomonte to become director. his new creation.</p>
+                      <strong>Cooperation to lead</strong>
+                      <p>The information that Michele Gaudiomonte becomes the new creative director has been respectfully announced by SPOES STORE to the fashion industry and the public at Aristino Fashion Show Summer 2019 - the most important event of the year for this brand. At the same time, the company also announced the first collection of Michele in Vietnam.</p>
+                      <p>Talking about the chance to touch the Vietnamese fashion village, Michele said: "Three years ago, when I came to Vietnam for the first time, I was invited to attend Fashion Week. When Aristino asked me to collaborate with them, For me, it is a great joy because living in Vietnam also means being able to exchange and promote my Italian culture."</p>
+                      <p>Sharing about his new development direction for the product, the talented designer believes that harmony plays an important role in his creativity: "In my opinion, all the charm of fashion begins with culture and history When those two elements are combined with your country's professionalism and excellent craftsmanship, in Vietnam it is possible to create a new style and a new experience of clothing. We have every right to expect breakthrough developments in the future."</p>
+                      <div class="image" style="text-align: center;">
+                        <img src="img/gt1.jpg" alt="">
+                        <p>Creative Director Michele Gaudiomonte and designers SPOES STORE.</p>
+                      </div>
+                      <p>SPOES STORE places high expectations on this cooperation. After gaining a foothold in the competitive Vietnamese market, SPOES STORE continues to realize the desire to bring the Vietnamese domestic fashion brand into an international brand, with high quality and diversity. , flexible in product style.</p>
+                      <p>With bold steps and long-term strategic vision, SPOES STORE has been and continues to create new breakthroughs, helping to elevate Vietnamese men's fashion in general and the position of this brand in particular, in fashion market context has more and more opportunities but still lacks a leader.</p>
+                      
+                  </div>
+                </div>
+                <img src="img/bnn3.jpg" alt="" width="1500px">
+
+
+</div>
